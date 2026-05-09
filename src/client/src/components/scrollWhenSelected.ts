d539aa2f96a5f7484f4941a1d4818accdf87b9b9
Federico Jaramillo Martinez <federicojaramillom@gmail.com>
2026-05-10 00:20:05 +0200

    Fix scroll directive lint errors

--- src/client/src/components/scrollWhenSelected.ts
+++ src/client/src/components/scrollWhenSelected.ts
@@ -13,7 +13,7 @@ class ScrollWhenSelectedDirective extends Directive {
   override update(part: Part, [selected, key]: [boolean, unknown?]) {
     if (!this.isElementPart) throw new Error("scrollWhenSelected must be used on an element");
     if (selected && (!this.wasSelected || key !== this.previousKey)) {
-      const element = (part as Part & { element?: Element }).element;
+      const element = "element" in part ? part.element : undefined;
       requestAnimationFrame(() => {
         if (element instanceof HTMLElement) element.scrollIntoView({ block: "nearest" });
       });
@@ -23,7 +23,9 @@ class ScrollWhenSelectedDirective extends Directive {
     return undefined;
   }
 
-  override render(_selected: boolean, _key?: unknown) {
+  override render(selected: boolean, key?: unknown) {
+    void selected;
+    void key;
     return undefined;
   }
 }
