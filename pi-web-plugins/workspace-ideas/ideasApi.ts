3dec382cb1754d59603aaeed8b823333bebd25b8
Slava Iumin <zordok2@gmail.com>
2026-06-19 20:19:55 +0300

    refactor(workspace-ideas): move plugin package out of pi-web

--- pi-web-plugins/workspace-ideas/ideasApi.ts
+++
@@ -1,9 +0,0 @@
-import type { WorkspacePanelContext, WorkspacePanelSessionInfo } from "@jmfederico/pi-web/plugin-api";
-
-export async function writeWorkspaceFile(context: WorkspacePanelContext, path: string, content: string): Promise<void> {
-  await context.files.writeFile(path, content);
-}
-
-export async function startIdeaSession(context: WorkspacePanelContext, ideaId: string, prompt: string): Promise<WorkspacePanelSessionInfo> {
-  return await context.sessions.startWithPrompt(prompt, { newWorkspace: true, ideaId });
-}
